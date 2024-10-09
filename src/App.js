6ec9290b271b0683769a1cf673ee38dea41f60fa
Máté Galbavi <galbavi.mate@gmail.com>
2024-10-09 18:21:02 +0200

    Gyakorlo feladat 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Receipt from './Receipt';
+
+const recipes = [
+  {
+    id: 1,
+    name: "Rántotta",
+    ingredients: [
+      "2 tojás",
+      "egy csipet só"
+    ],
+    instructions: "Feltörjük, megsütjük, finom lesz.",
+    difficulty: 3
+  },
+  {
+    id: 1,
+    name: "Tükörtojás",
+    ingredients: [
+      "3 tojás",
+      "egy csipet só",
+      "zsír"
+    ],
+    instructions: "Feltörjük, megsütjük, megesszük.",
+    difficulty: 4
+  },
+  {
+    id: 1,
+    name: "Kávé",
+    ingredients: [
+      "kávé",
+      "2 dl tej",
+      "1 kocka cukor"
+    ],
+    instructions: "Megfőzzük, ízesítjük.",
+    difficulty: 4
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        {recipes.map(item => {
+          return (<Receipt key={item.id} data={item}/>)
+        })}
+      </table>
     </div>
   );
 }
